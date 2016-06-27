7f988726a79d05287292e0c22c923ffb9663a0ab
Leonardo <leo.dimaz@gmail.com>
2016-06-27 18:57:43 +0200

    Update control.py

--- control.py
+++ control.py
@@ -1,16 +1,18 @@
 from BeautifulSoup import BeautifulSoup
 import requests
 import os.path
-
-#sezione url
-url = "link didattica uniroma2 da controllare" #http://didattica.uniroma2.it/files/index/insegnamento/nomedelcorso
-#sezione gmail (per ora funziona solamente con questo provider di posta)
-fromaddr = "youremail"
-toaddrs  = "youremail"
+#sezione multiurl, includere la parte finale /id/desc/9999
+urls = [
+'http://didattica.uniroma2.it/files/index/insegnamento/nomeinsegnamento1/id/desc/9999',
+'http://didattica.uniroma2.it/files/index/insegnamento/nomeinsegnamento2/id/desc/9999'
+]
+#section gmail
+fromaddr = "tuaemail"
+toaddrs  = "tuaemail"
 username = "username"
 password = "password"
 
-def sendmail(name, link):
+def sendmail(name, link, url):
 	import smtplib
 	msg = "[  ] Potrebbero essere usciti i risultati\n[  ] Titolo delll'ultimo file inserito: "+str(name)+"\n[+] "+str(link)+"\n[+] "+url
 	server = smtplib.SMTP("smtp.gmail.com:587")
@@ -19,33 +21,36 @@ def sendmail(name, link):
 	server.sendmail(fromaddr, toaddrs, msg)
 	server.quit()
 
-def check():
+def check(url, line):
 	file = open("db.txt","r")
-	lines = file.read()[0]
+	lines = file.read().split("\n")[line]
 	r=requests.get(url)
 	soup = BeautifulSoup(r.text)
 	trs = [tr.find("td").find("a") for tr in soup.findAll('tr',attrs={'class':'even'})]
+	#print str(len(trs))+">"+str(int(lines))+"?"
 	if len(trs)>int(lines):
 		last_insert = trs[0].text
 		last_insert_href = "http://didattica.uniroma2.it"+trs[0]['href']
-		sendmail(last_insert, last_insert_href)
-		first()
+		sendmail(last_insert, last_insert_href, url)
+		first(url)
 
-def first():
-	file = open("db.txt","w")
+def first(url):
+	file = open("db.txt","a")
 	r=requests.get(url)
 	soup = BeautifulSoup(r.text)
 	trs = [tr.text for tr in soup.findAll('tr',attrs={'class':'even'})]
-	file.write(str(len(trs)))
+	file.write(str(len(trs))+"\n")
 	file.close()
-	check()
 
 def main():
+	current_line = 0
 	if os.path.isfile("db.txt"):
-		check()
+		for url in urls:
+			check(url, current_line)
+			current_line = current_line + 1
 	else:
-		first()
+		for url in urls:
+			first(url)
 
 if __name__ == "__main__":
 	main()
-
